d3948385f9505695315c36ba7565d27f19c5a0f7
niels1921 <nielsie21@live.nl>
2018-09-18 20:17:38 +0200

    Nodes

---
+++ AmazonSimulator VS/Models/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public bool Occupied { get; set; }
+
+    }
+}
