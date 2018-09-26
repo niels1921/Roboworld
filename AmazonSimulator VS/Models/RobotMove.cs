9a9ec132d71a683434c85b3b93d090fd283e1b48
Harm van den Bogert <harm.vandenbogert@outlook.com>
2018-09-26 10:54:38 +0200

    robot interface

---
+++ AmazonSimulator VS/Models/RobotMove.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RobotMove : IRobotTask
+    {
+        private bool StartupComplete = false;
+        private bool Complete = false;
+
+        private List<Node> Path;
+
+        public void StartTask(Robot r)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Taskcomplete(Robot r)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
