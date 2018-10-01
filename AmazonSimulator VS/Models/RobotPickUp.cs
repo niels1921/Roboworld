65e1ee118f908e01678fcc1b038d0b95e5438ff9
niels1921 <nielsie21@live.nl>
2018-10-01 12:39:19 +0200

    het gaat ooit werken

--- AmazonSimulator VS/Models/RobotPickUp.cs
+++ AmazonSimulator VS/Models/RobotPickUp.cs
@@ -7,19 +7,23 @@ namespace Models
 {
     public class RobotPickUp : IRobotTask
     {
-        public RobotPickUp()
+        private Shelf shelf;
+
+        public RobotPickUp(Shelf s)
         {
-            
+            this.shelf = s;
         }
 
         public void StartTask(Robot r)
         {
-            throw new NotImplementedException();
+            r.AddShelf(this.shelf);
         }
 
         public bool Taskcomplete(Robot r)
         {
-            return true;
+            double xwaarde = Math.Round(r.x, 2);
+            double zwaarde = Math.Round(r.z, 2);
+            return xwaarde == Math.Round(shelf.x, 2) && zwaarde == Math.Round(shelf.z, 2);
         }
     }
 }
